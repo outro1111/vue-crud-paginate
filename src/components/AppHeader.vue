f709708ccd9068f5a6f6361e0ee604673f770628
outro1111 <67306701+outro1111@users.noreply.github.com>
2023-01-27 09:28:26 +0900

    Update AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,9 +2,11 @@
   <div id="header">
     <h1><router-link to="/">Vue CRUD</router-link></h1>
     <nav>
-      <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="/list">List</router-link></li>
-      <li><router-link to="/list2" @click="forceRerender">List2</router-link></li>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/list">List</router-link></li>
+        <li><router-link to="/list2" @click="forceRerender">List2</router-link></li>
+      </ul>
     </nav>
   </div>
 </template>
@@ -18,7 +20,7 @@ export default {
 <style scoped>
 #header {display:flex;padding:30px 0 10px;align-items:center}
 #header h1 a {font-size:28px;font-weight:800;color:#58e4a5;}
-#header nav {display:flex;margin-left:2rem}
+#header nav ul {display:flex;margin-left:2rem}
 #header nav a {padding:0 15px;color:var(--font-white);font-size:16px}
 #header nav a.router-link-active {color:#58e4a5;font-weight:800}
 </style>
