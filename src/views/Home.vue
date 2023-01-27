55ddd74cae68dc54b77ecdc59a224a46bab10c4d
outro1111 <67306701+outro1111@users.noreply.github.com>
2023-01-27 09:31:04 +0900

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
   <div class="home">
     <h2 class="title">Vue CRUD</h2>
     <div class="visual"><img src="@/assets/images/home.png" alt=""></div>
-    <p>CRUD는 대부분의 컴퓨터 소프트웨어가 가지는 기본적인 데이터 처리 기능인 Create(생성), Read(읽기), Update(갱신), Delete(삭제)를 묶어서 일컫는 말이다. 사용자 인터페이스가 갖추어야 할 기능(정보의 참조/검색/갱신)을 가리키는 용어로서도 사용된다.</p>
+    <p>CRUD는 대부분의 컴퓨터 소프트웨어가 가지는 기본적인 데이터 처리 기능인 Create(생성), Read(읽기), Update(갱신), Delete(삭제)를 묶어서 일컫는 말이다. 사용자 인터페이스가 갖추어야 할 기능(정보의 참조/검색/갱신)을 가리키는 용어로서도 사용된다. <br />
+    json-server --watch data/db.json</p>
   </div>
 </template>
 
